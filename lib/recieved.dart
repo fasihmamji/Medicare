4647afa05f19e141a6ba855d58639fc3d9f722fe
Fasih Mamji <k190139@nu.edu.pk>
2021-12-15 18:23:14 +0500

    Reciever End

--- lib/recieved.dart
+++ lib/recieved.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
 
-class Recieved extends StatelessWidget {
+class Recieved extends StatefulWidget {
+  @override
+  State<Recieved> createState() => _RecievedState();
+}
+
+class _RecievedState extends State<Recieved> {
+  String? valueChoose;
+
+  List listItem = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Recieved'),
+      body: Container(
+        padding: EdgeInsets.only(left: 16, right: 16),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.blueGrey,
+            width: 3,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: DropdownButton(
+          hint: Text('Select Item'),
+          dropdownColor: Colors.blueGrey,
+          icon: Icon(Icons.arrow_drop_down),
+          iconSize: 30,
+          isExpanded: true,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+          ),
+          value: valueChoose,
+          onChanged: (newValue) {
+            setState(() {
+              valueChoose = newValue as String;
+            });
+          },
+          items: listItem.map((valueItem) {
+            return DropdownMenuItem(
+              value: valueItem,
+              child: Text(valueItem),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
